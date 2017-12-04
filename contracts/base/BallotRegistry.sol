acb60c175c3c46a3fa0a6227e6cb10078d9bd382
Steven Landers <steven.landers@gmail.com>
2017-12-03 19:48:49 -0500

    adding copyright header

--- contracts/base/BallotRegistry.sol
+++ contracts/base/BallotRegistry.sol
@@ -1,3 +1,22 @@
+// ------------------------------------------------------------------------------
+// This file is part of netvote.
+//
+// netvote is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// netvote is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with solidity.  If not, see <http://www.gnu.org/licenses/>
+//
+// (c) 2017 netvote contributors.
+//------------------------------------------------------------------------------
+
 pragma solidity ^0.4.17;
 
 import "../ElectionPhaseable.sol";
