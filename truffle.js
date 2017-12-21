10fab89a3c3dac84a580952f1eed22833a1c827f
Steven Landers <steven.landers@gmail.com>
2017-12-21 17:35:04 -0500

    adding code coverage

--- truffle.js
+++ truffle.js
@@ -15,6 +15,12 @@ module.exports = {
             provider: new HDWalletProvider(ropstenCreds.mneumonic, "https://ropsten.infura.io/"+ropstenCreds.accesskey),
             network_id: 3
         },
+        testing: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*",
+            gas: 2e7,
+        },
         ganache: {
             host: "127.0.0.1",
             port: 7545,
