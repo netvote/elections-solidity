c6e5d4e4a7391310ae89f4bc213bdcfc3cda42cf
Steven Landers <steven.landers@gmail.com>
2018-02-27 15:01:29 -0500

    add token voting (#23)

---
+++ contracts/elections/token/TokenElection.sol
@@ -0,0 +1,79 @@
+// ------------------------------------------------------------------------------
+// This file is part of netvote.
+//
+// netvote is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// netvote is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with solidity.  If not, see <http://www.gnu.org/licenses/>
+//
+// (c) 2017 netvote contributors.
+//------------------------------------------------------------------------------
+
+pragma solidity ^0.4.17;
+
+import "../basic/BasicElection.sol";
+
+
+/**
+ * @title TokenElection
+ * @dev Allows token holders to vote
+ */
+contract TokenElection is BasicElection {
+
+    address public tokenAddress;
+    uint public balanceTime;
+
+    /**
+      * @dev Constructor for creating a BasicElection
+      * @param hashedUserId bytes32 sha3 hash of external userId
+      * @param allowanceAddress The address of global allowance contract.
+      * @param ownerOfAllowance The owner of the Vote Allowance to deduct from.
+      * @param allowUpdates Whether to allow Voters to update their vote.
+      * @param revealerAddress The address of the Key Revealer that will post the Key.
+      * @param location The location reference for the ballot metadata (e.g., IPFS Reference)
+      * @param gatewayAddress The address of the Vote Gateway that will submit votes.
+      * @param autoActivate Automatically activate this election to allow votes
+      * @param erc20Token Address of token whose holders are voting
+      * @param balanceTimestamp Timestamp for balance evaluation for weighted voting
+      */
+    function TokenElection(
+        bytes32 hashedUserId,
+        address allowanceAddress,
+        address ownerOfAllowance,
+        bool allowUpdates,
+        address revealerAddress,
+        string location,
+        address gatewayAddress,
+        bool autoActivate,
+        address erc20Token,
+        uint balanceTimestamp) BasicElection(hashedUserId, allowanceAddress, ownerOfAllowance, allowUpdates, revealerAddress, location, gatewayAddress, false) public
+    {
+        require(erc20Token != address(0));
+        require(isContract(erc20Token));
+        electionType = "TOKEN";
+        tokenAddress = erc20Token;
+
+        //sanity check, must be a date within last 5 years
+        require(balanceTimestamp > (now - (5 years)));
+        balanceTime = balanceTimestamp;
+
+        if (autoActivate) {
+            activate();
+        }
+    }
+
+    function isContract(address addr) internal constant returns (bool) {
+        uint size;
+        assembly { size := extcodesize(addr) }
+        return size > 0;
+    }
+
+}
---
+++ test/contracts/TokenElection.js
@@ -0,0 +1,68 @@
+// ------------------------------------------------------------------------------
+// This file is part of netvote.
+//
+// netvote is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// netvote is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with solidity.  If not, see <http://www.gnu.org/licenses/>
+//
+// (c) 2017 netvote contributors.
+//------------------------------------------------------------------------------
+
+let TokenElection = artifacts.require("TokenElection");
+let Vote = artifacts.require("Vote");
+
+
+let assertThrowsAsync = async (fn, regExp) => {
+    let f = () => {};
+    try {
+        await fn();
+    } catch(e) {
+        f = () => {throw e};
+    } finally {
+        assert.throws(f, regExp);
+    }
+};
+
+contract('TokenElection constructor', function (accounts) {
+    let owner;
+    let admin;
+    let notAdmin;
+    let validAddr;
+    let token;
+
+    before(async () => {
+        owner = accounts[0];
+        admin = accounts[1];
+        notAdmin = accounts[2];
+        validAddr = accounts[3];
+        token = await Vote.new({from: owner});
+    });
+
+    it("should construct", async function () {
+        let balanceDate = new Date().getTime()/1000;
+        await TokenElection.new("uid", validAddr, owner, true, validAddr, "metadata", validAddr, true, token.address, balanceDate, {from: owner});
+    });
+
+    it("should not allow ancient balance date", async function () {
+        await assertThrowsAsync(async ()=>{
+            await TokenElection.new(validAddr,owner, true, validAddr, "metadata", validAddr, true, token.address, 0, {from: owner})
+        }, /Exception/);
+    });
+
+    it("should not allow non-contract token", async function () {
+        let balanceDate = new Date().getTime()/1000;
+        await assertThrowsAsync(async ()=>{
+            await TokenElection.new(validAddr,owner, true, validAddr, "metadata", validAddr, true, validAddr, balanceDate, {from: owner})
+        }, /Exception/);
+    });
+
+});
