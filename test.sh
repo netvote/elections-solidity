85e6bd2e6d6cb88789d40c928a9977c69b64f776
Steven Landers <steven.landers@gmail.com>
2018-06-03 19:35:33 -0400

    updating web3 version

--- test.sh
+++ test.sh
@@ -5,7 +5,10 @@ set -e
 ganache-cli --gasLimit 200000000 2> /dev/null 1> /dev/null &
 sleep 5 # to make sure ganache-cli is up and running before compiling
 rm -rf build
-truffle compile
+echo "compiling..."
+truffle compile --network development
+echo "migrating..."
 truffle migrate --reset --network development
-truffle test
+echo "testing..."
+truffle test --network development
 kill -9 $(lsof -t -i:8545)
