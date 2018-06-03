9cad7a91b386e359956dd6ec8fa068079b15def7
Steven Landers <steven.landers@gmail.com>
2018-06-03 19:25:47 -0400

    updating port

--- truffle.js
+++ truffle.js
@@ -16,7 +16,7 @@ module.exports = {
     networks: {
         development: {
             host: "localhost",
-            port: 9545,
+            port: 8545,
             network_id: "*", // Match any network id
             gas: 2e7,
         },
