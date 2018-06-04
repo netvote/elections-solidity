b440d70187f102b9bea53eb5891141c629315362
Steven Landers <steven.landers@gmail.com>
2018-06-03 21:54:40 -0400

    parallelizing

--- test.sh
+++ test.sh
@@ -2,10 +2,14 @@
 
 set -e
 
-ganache-cli --gasLimit 200000000 2> /dev/null 1> /dev/null &
-sleep 5 # to make sure ganache-cli is up and running before compiling
-echo "PORT 8545 PID: $(lsof -t -i:8545)"
-rm -rf build
-echo "starting tests..."
-truffle test --network development
-kill -9 $(lsof -t -i:8545)
+if [ $1 = "lint" ]; then
+    npm run lint
+elif [ $1 = "coverage" ]; then
+    npm run coverage
+    codecov
+elif [ $1 = "gas" ]; then
+    ganache-cli --gasLimit 200000000 2> /dev/null 1> /dev/null &
+    sleep 5
+    npm run test-gas
+    killall ganache-cli || true
+fi
