99fe5b96885e08478217d252dc6ea2e7cf389703
Steven Landers <steven.landers@gmail.com>
2018-06-22 01:17:23 -0400

    fix tests

--- test/end-to-end/jslib/basic-election.js
+++ test/end-to-end/jslib/basic-election.js
@@ -145,7 +145,7 @@ let createTokenElection = async(config) => {
         numVotes = Object.keys(config.voters).length;
     }
     await config.allowanceContract.transfer(config.contract.address, web3.toWei(numVotes+1, 'ether'), {from: config.account.owner})
-    await config.allowanceContract.addElection(config.contract.address, {from: config.account.owner})
+    await config.allowanceContract.addElection(config.contract.address, {from: config.netvote})
     config = await measureGas(config, "Allowance: authorize election");
     return config;
 };
