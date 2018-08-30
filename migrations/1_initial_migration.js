7216e24c69ea1755321c111b18195c5c1ed65c8d
Steven Landers <steven.landers@gmail.com>
2018-08-30 15:35:46 -0400

    add mainnet deployments (#43)

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,16 @@
 var Migrations = artifacts.require("./Migrations.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+const snooze = (ms) => { 
+  console.log("sleeping "+ms)
+  return new Promise(resolve => setTimeout(resolve, ms)); 
+}
+
+module.exports = function(deployer, network) {
+  let sleepMs = (network === "mainnet") ? 60000 : 1;
+  deployer.then(async()=>{
+    await deployer.deploy(Migrations);
+    await snooze(sleepMs);
+  }).catch((e)=>{
+    throw e;
+  })
 };
--- migrations/2_address_set_migration.js
+++ migrations/2_address_set_migration.js
@@ -7,7 +7,21 @@ let BallotRegistry = artifacts.require("./elections/links/BallotRegistry.sol");
 let PoolRegistry = artifacts.require("./elections/links/PoolRegistry.sol");
 let BasicElection = artifacts.require("./elections/basic/BasicElection.sol");
 
-module.exports = function(deployer) {
-    deployer.deploy(AddressSet);
-    deployer.link(AddressSet, [TieredElection, TieredBallot, TieredPool, BasePool, BasicElection, BallotRegistry, PoolRegistry]);
+const snooze = (ms) => { 
+    console.log("sleeping "+ms)
+    return new Promise(resolve => setTimeout(resolve, ms)); 
+}
+
+module.exports = async (deployer, network) => {
+    let sleepMs = (network === "mainnet") ? 60000 : 1;
+
+    deployer.then(async() => {
+        await snooze(sleepMs)
+        await deployer.deploy(AddressSet)
+        await snooze(sleepMs)
+        await deployer.link(AddressSet, [TieredElection, TieredBallot, TieredPool, BasePool, BasicElection, BallotRegistry, PoolRegistry])
+        await snooze(sleepMs)
+    }).catch((e)=>{
+        throw e;
+    })
 };
